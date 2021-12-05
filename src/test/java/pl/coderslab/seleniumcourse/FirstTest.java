0501388dd67df2b32347bf4b0cbe640339c36898
wasijczuk <j.wasijczuk@gmail.com>
2021-12-05 11:18:19 +0100

    first selenium test. search with qwant

---
+++ src/test/java/pl/coderslab/seleniumcourse/FirstTest.java
@@ -0,0 +1,17 @@
+package pl.coderslab.seleniumcourse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.qwant.com/");
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("coders lab");
+        searchInput.submit();
+    }
+}
